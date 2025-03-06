78f71a7b24e347bd3b4c1c3c5ffa7d8325f8db57
Maria Gabriella <mgsc@cesar.school>
2025-03-06 12:02:44 -0300

    Primeiro commit

---
+++ src/main/java/dio/springboot/PrimeirospassosApplication.java
@@ -0,0 +1,12 @@
+package dio.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeirospassosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeirospassosApplication.class, args);
+	}
+}
